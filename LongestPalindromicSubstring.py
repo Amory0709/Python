f7e043c1a3610f57ed1b02c88c682e7307005481
Amory0709 <37529329+Amory0709@users.noreply.github.com>
2020-06-10 15:51:04 -0400

    Update LongestPalindromicSubstring.py

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -27,3 +27,37 @@ class Solution:
                     left, right, maxLength = start, end, step+1
                     
         return s[left:right+1]
+    
+class Solution2:
+    """
+    @param s: input string
+    @return: a string as the longest palindromic substring
+    """
+    def longestPalindrome(self, s):
+        # Time O(n^2) Space O(1)
+        if not s:
+            return ''
+            
+        n = len(s) 
+        longest = ''
+        for mid in range(n):
+            sub = self.findPalindrome(mid, mid, s)
+            if len(sub) > len(longest):
+                longest = sub
+            sub = self.findPalindrome(mid, mid+1, s)
+            if len(sub) > len(longest):
+                longest = sub
+
+        return longest 
+        
+    def findPalindrome(self, start, end, s):
+        while 0 <= start and end < len(s):
+            if s[start] != s[end]:
+                break
+            start -= 1
+            end += 1
+            
+        return s[start+1:end] 
+        # error: [start:end+1] 
+        # since start and end is different so we should not use s[start] and s[end]
+
